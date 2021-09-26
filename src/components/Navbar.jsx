886c28f397ff40dce5ccbe3ff6434d4c602ece66
Ben <ben.lemaitre@protonmail.com>
2021-09-27 00:17:39 +0200

    done homepage and navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,60 @@
 import React from "react";
-import { HStack, Text } from "@chakra-ui/layout";
 import { Link } from "react-router-dom";
+
+import Logo from "../logo.svg";
+
+import {
+  Box,
+  Flex,
+  Text,
+  Button,
+  Stack,
+  Image,
+  useColorModeValue,
+  useBreakpointValue,
+} from "@chakra-ui/react";
 
 const Navbar = () => {
   return (
-    <HStack bg="gray.600" justifyContent="space-between">
-      <Link to="/">Home</Link>
-      <Text>0x00000</Text>
-    </HStack>
+    <Box>
+      <Flex
+        bg={useColorModeValue("white", "gray.800")}
+        color={useColorModeValue("gray.600", "white")}
+        minH={"60px"}
+        py={{ base: 2 }}
+        px={{ base: 4 }}
+        borderBottom={1}
+        borderStyle={"solid"}
+        borderColor={useColorModeValue("gray.200", "gray.900")}
+        align={"center"}
+      >
+        <Flex flex={{ base: 1 }} justify={{ base: "center", md: "start" }}>
+          <Link to="/">
+            <Flex direction="row" align={"center"}>
+              <Image src={Logo} boxSize="48px" alt="logo" />
+              <Text
+                textAlign={useBreakpointValue({ base: "center", md: "left" })}
+                fontFamily={"heading"}
+                fontSize="x-large"
+                color={useColorModeValue("gray.800", "white")}
+              >
+                DCentra-Funding
+              </Text>
+            </Flex>
+          </Link>
+        </Flex>
+        <Stack
+          flex={{ base: 1, md: 0 }}
+          justify={"flex-end"}
+          direction={"row"}
+          spacing={6}
+        >
+          <Button as={"a"} fontSize={"sm"} fontWeight={400} variant={"link"}>
+            0x00000000000
+          </Button>
+        </Stack>
+      </Flex>
+    </Box>
   );
 };
 
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,23 +1,61 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Center, Button, ButtonGroup } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Stack,
+  Button,
+  Text,
+  useBreakpointValue,
+} from "@chakra-ui/react";
 
 const Homepage = () => {
   return (
-    <Center h="100px">
-      <ButtonGroup spacing="6" width="250px">
-        <Link to="/projects">
-          <Button colorScheme="teal" size="sm" variant="outline">
-            Discover
-          </Button>
-        </Link>
-        <Link to="/create">
-          <Button colorScheme="teal" size="sm">
-            Start Project
-          </Button>
-        </Link>
-      </ButtonGroup>
-    </Center>
+    <Flex w={"full"} h={"100vh"} justifyContent={"center"}>
+      <Stack
+        w={"full"}
+        as={Box}
+        textAlign={"center"}
+        spacing={6}
+        py={{ base: 20, md: 36 }}
+        maxW={"2xl"}
+      >
+        <Text
+          fontWeight={700}
+          lineHeight={1.2}
+          fontSize={useBreakpointValue({ base: "3xl", md: "4xl" })}
+        >
+          Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod
+          tempor
+        </Text>
+        <Text>
+          Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod
+          tempor. Lorem ipsum dolor sit amet consectetur adipiscing elit sed do
+          eiusmod tempor
+        </Text>
+        <Stack direction={"row"} spacing={6} alignSelf={"center"}>
+          <Link to="/projects">
+            <Button fontSize={"sm"} fontWeight={400} variant={"outline"}>
+              Discover
+            </Button>
+          </Link>
+          <Link to="/create">
+            <Button
+              display={{ base: "none", md: "inline-flex" }}
+              fontSize={"sm"}
+              fontWeight={600}
+              color={"white"}
+              bg={"green.400"}
+              _hover={{
+                bg: "green.300",
+              }}
+            >
+              Start Project
+            </Button>
+          </Link>
+        </Stack>
+      </Stack>
+    </Flex>
   );
 };
 
