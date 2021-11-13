835631b13c13dc8de434f5b25f382f7dbb4c58d3
Ben <ben.lemaitre@protonmail.com>
2021-11-13 15:46:46 +0100

    handling non-ETH browsers

--- src/libs/utils.js
+++ src/libs/utils.js
@@ -5,12 +5,12 @@ export const loadWeb3 = async () => {
   if (window.ethereum) {
     window.web3 = new Web3(window.ethereum)
     await window.ethereum.request({ method: 'eth_requestAccounts' })
+    return true
   } else if (window.web3) {
     window.web3 = new Web3(window.web3.currentProvider)
+    return true
   } else {
-    window.alert(
-      'Non-ethereum browser detected. You should consider using metamask'
-    )
+    return false
   }
 }
 
--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,38 +1,57 @@
 import { useEffect, useState } from 'react'
+import Image from 'next/image'
 import { AnimatePresence } from 'framer-motion'
 import { ChakraProvider } from '@chakra-ui/provider'
-import { Spinner } from '@chakra-ui/react'
+import { Spinner, Text, Heading, Stack } from '@chakra-ui/react'
 
 import Layout from '../components/layouts/main'
 import theme from '../libs/theme'
+import metamaskError from '../../public/metamask-error.svg'
 
 import { loadWeb3 } from '../libs/utils'
 
 const Website = ({ Component, pageProps, router }) => {
   const [loading, setLoading] = useState(true)
+  const [hasMetamask, setHasMetamask] = useState(false)
 
   useEffect(() => {
     const load = async () => {
-      await loadWeb3()
+      setHasMetamask(await loadWeb3())
       setLoading(false)
     }
 
     load()
   }, [])
 
-  return (
-    <ChakraProvider theme={theme}>
-      <Layout router={router}>
+  if (hasMetamask) {
+    return (
+      <ChakraProvider theme={theme}>
+        <Layout router={router}>
+          <AnimatePresence exitBeforeEnter initial={true}>
+            {loading ? (
+              <Spinner size="xl" position="absolute" left="50%" top="50%" />
+            ) : (
+              <Component {...pageProps} key={router.route} />
+            )}
+          </AnimatePresence>
+        </Layout>
+      </ChakraProvider>
+    )
+  } else {
+    return (
+      <ChakraProvider theme={theme}>
         <AnimatePresence exitBeforeEnter initial={true}>
-          {loading ? (
-            <Spinner size="xl" position="absolute" left="50%" top="50%" />
-          ) : (
-            <Component {...pageProps} key={router.route} />
-          )}
+          <Stack alignItems="center" alignSelf="center" p={12}>
+            <Heading size="md">Welcome to Dcentra-Funding</Heading>
+            <Image src={metamaskError} alt="metamask error" width="670px" />
+            <Text fontWeight="bold">
+              Non-ethereum browser detected. You should consider using metamask
+            </Text>
+          </Stack>
         </AnimatePresence>
-      </Layout>
-    </ChakraProvider>
-  )
+      </ChakraProvider>
+    )
+  }
 }
 
 export default Website
